e630d6c9ec2121299e70f401ba5a3aa9d08f2c8e
mktcode <kontakt@markus-kottlaender.de>
2018-07-03 14:29:31 +0200

    converted to ES5

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-import sc2 from 'sc2-sdk'
+'use strict';
 
-const VueSteemConnect = {
-  install (Vue, _options) {
-    const options = Object.assign({}, _options, {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _sc2Sdk = require('sc2-sdk');
+
+var _sc2Sdk2 = _interopRequireDefault(_sc2Sdk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var VueSteemConnect = {
+  install: function install(Vue, _options) {
+    var options = Object.assign({}, _options, {
       scope: _options.scope || ['vote', 'comment']
     });
 
-    Vue.prototype.$steemconnect = sc2.Initialize({
+    Vue.prototype.$steemconnect = _sc2Sdk2.default.Initialize({
       app: options.app,
       callbackURL: options.redirectUrl,
-      scope: options.scope || ['vote', 'comment']
-    })
+      scope: options.scope
+    });
   }
-}
+};
 
-export default VueSteemConnect
+exports.default = VueSteemConnect;
