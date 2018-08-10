dbf9383a3a03f07fafea7d325f6dcbaf69902cdc
mktcode <kontakt@markus-kottlaender.de>
2018-08-10 13:22:31 +0200

    added store module

--- dist/index.js
+++ dist/index.js
@@ -27,6 +27,60 @@ var VueSteemConnect = {
     Vue.SteemConnect = function () {
       return api;
     };
+
+    // store module
+    Vue.SteemConnectStore = {
+      state: {
+        user: null // steemconnect user
+      },
+      getters: {
+        user: function user(state) {
+          return state.user;
+        }
+      },
+      mutations: {
+        login: function login(state, user) {
+          state.user = user;
+        },
+        logout: function logout(state) {
+          state.user = null;
+        }
+      },
+      actions: {
+        login: function login(_ref) {
+          var commit = _ref.commit,
+              dispatch = _ref.dispatch,
+              state = _ref.state;
+
+          return new Promise(function (resolve, reject) {
+            // user will be set, when coming from auth page
+            // but not if accessed this page directly
+            if (!state.user) {
+              // in that case we look for an access token in localStorage
+              var accessToken = localStorage.getItem('access_token');
+              if (accessToken) {
+                // set access token and try to fetch user object
+                Vue.SteemConnect().setAccessToken(accessToken);
+                Vue.SteemConnect().me(function (err, user) {
+                  if (err) reject(err);else {
+                    // save user object in store
+                    commit('login', user);
+                    resolve();
+                  }
+                });
+              }
+            }
+          });
+        },
+        logout: function logout(_ref2) {
+          var commit = _ref2.commit;
+
+          // remove access token and unset user in store
+          localStorage.removeItem('access_token');
+          commit('logout');
+        }
+      }
+    };
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -17,6 +17,55 @@ const VueSteemConnect = {
     Vue.SteemConnect = () => {
       return api;
     }
+
+    // store module
+    Vue.SteemConnectStore = {
+      state: {
+        user: null // steemconnect user
+      },
+      getters: {
+        user (state) {
+          return state.user
+        }
+      },
+      mutations: {
+        login (state, user) {
+          state.user = user
+        },
+        logout (state) {
+          state.user = null
+        }
+      },
+      actions: {
+        login ({ commit, dispatch, state }) {
+          return new Promise((resolve, reject) => {
+            // user will be set, when coming from auth page
+            // but not if accessed this page directly
+            if (!state.user) {
+              // in that case we look for an access token in localStorage
+              const accessToken = localStorage.getItem('access_token')
+              if (accessToken) {
+                // set access token and try to fetch user object
+                Vue.SteemConnect().setAccessToken(accessToken)
+                Vue.SteemConnect().me((err, user) => {
+                  if (err) reject(err)
+                  else {
+                    // save user object in store
+                    commit('login', user)
+                    resolve()
+                  }
+                })
+              }
+            }
+          })
+        },
+        logout ({ commit }) {
+          // remove access token and unset user in store
+          localStorage.removeItem('access_token')
+          commit('logout')
+        }
+      }
+    }
   }
 }
 
